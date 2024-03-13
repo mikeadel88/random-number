a171f2a7614566e83514e96bfb32533820e16b06
mikadel88 <michael2016292@hotmail.com>
2024-03-13 16:44:47 +0200

    PWA

--- script.js
+++ script.js
@@ -31,5 +31,13 @@ resetMe.addEventListener("click",()=>{
     numberArr.innerHTML= "" ;
 
 })
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
 
 
